bd5ca7169ba9727b12f6aa82aeacc17deecbe062
Rasika Kale <rkale@ucsc.edu>
2019-05-14 14:12:28 -0700

    Added all buttons to Grad Planner

--- app/src/main/java/com/example/mainproject/secondYear.java
+++ app/src/main/java/com/example/mainproject/secondYear.java
@@ -1,7 +1,9 @@
 package com.example.mainproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class secondYear extends AppCompatActivity {
 
@@ -10,4 +12,11 @@ public class secondYear extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second_year);
     }
-}
+
+    public void backToPlanner(View view) {
+        Intent intent = new Intent(this, GradPlanner.class);
+        startActivity(intent);
+    }
+
+    }
+
