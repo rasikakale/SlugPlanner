62dee086027cdb4f9addcc732deb321604510b0a
Rasika Kale <rkale@ucsc.edu>
2019-05-19 15:18:58 -0700

    Saved data of Major and Grad Date

---
+++ app/src/main/java/com/example/mainproject/EditProfile.java
@@ -0,0 +1,50 @@
+package com.example.mainproject;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class EditProfile extends AppCompatActivity {
+
+    EditText majorInput;
+    EditText gradDateInput;
+    SharedPreferences prefs;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile);
+        setTitle("Edit Your Profile");
+
+        prefs = getSharedPreferences("mydata", MODE_PRIVATE);
+
+        String major = prefs.getString("my_major", "Enter major");
+        String gradDate = prefs.getString("my_date", "Enter date");
+
+        majorInput = findViewById(R.id.editmajor);
+        gradDateInput = findViewById(R.id.editDate);
+
+        majorInput.setText(major);
+        gradDateInput.setText(gradDate);
+    }
+
+    public void save(View view) {
+
+        String major = majorInput.getText().toString();
+        String gradDate = gradDateInput.getText().toString();
+
+        //save Data
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString("my_major", major);
+        editor.putString("my_date", gradDate);
+        editor.apply();
+
+
+        Intent intent =  new Intent(this, Profile.class);
+        startActivity(intent);
+
+    }
+}
